2837658041cb536ed012ffa8435ebb3336e15839
LoveLoliii <woshigegefengleng@live.com>
2016-04-18 13:41:16 +0800

    Revert "page2布局修改"
    This reverts commit e5656f89db1021a9432bc933eadc6a1baf84373b.

--- app/src/main/java/com/ss/mrm/LoginActivity.java
+++ app/src/main/java/com/ss/mrm/LoginActivity.java
@@ -57,8 +57,7 @@ public class LoginActivity extends AppCompatActivity {
             }
             Log.i("main", "用户名:" + strUserName + "密码：" + strPassword);
              Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-            startActivity(intent);
-           // overridePendingTransition(R.anim.in_from_right, R.anim.out_to_left);
+             startActivity(intent);
         }
     }
 }
